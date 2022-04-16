8f0adc768e3a5e325a548d12d2dd2e62c52e87e9
codingsake <thinthazin.chris@gmail.com>
2022-04-16 01:13:19 -0400

    Added on_press and on_release function to monitor user's input

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import pynput
 
 from pynput.keyboard import Key, Listener 
+
+#This function will print the key that the user pressed.
+def on_press(key):
+    print(f"{key} pressed.")
+
+#This function will get out of Loop
+def on_release(key):
+    #check if the user input is esc key.
+    if key == Key.esc:
+    #if yes, get out of the loop
+        return False
+
+#Call on_press and on_release function with loop
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
